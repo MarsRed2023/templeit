a0d6040ea8718048d54639730b5c240fd726ae4d
Mars <151302717+MarsRed2023@users.noreply.github.com>
2025-02-10 16:54:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
-import speech_recognition as sr
-import time
+# Importar
+from flask import Flask, render_template
 
-mic = sr.Microphone()
-recog = sr.Recognizer()
-palabras = ["Casa", "Carro"]
-pword = []
-while True:
-    print("Tendras 5 segundos para decir la palabra")
-    try:
-        with mic as audio:
-            print("Wait a moment...")
-            recog.adjust_for_ambient_noise(audio)
-            print("Ya puedes hablar en 3 segundos")
-            time.sleep(3)
-            audio = recog.listen(audio, timeout=3)
-            texto = recog.recognize_google(audio, language="es-ES")
-            return render_template('index.html', audio = texto)
-    except:
-        print("Hubo un problema, intentalo de nuevo")
 
+app = Flask(__name__)
+
+def result_calculate(size, lights, device):
+    # Variables que permiten calcular el consumo energético de los aparatos
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5   
+    return size * home_coef + lights * light_coef + device * devices_coef 
+
+# La primera página
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# La segunda página
+@app.route('/<size>')
+def lights(size):
+    return render_template(
+                            'lights.html', 
+                            size=size
+                           )
+
+# La tercera página
+@app.route('/<size>/<lights>')
+def electronics(size, lights):
+    return render_template(
+                            'electronics.html',
+                            size = size, 
+                            lights = lights                           
+                           )
+
+# Cálculo
+@app.route('/<size>/<lights>/<device>')
+def end(size, lights, device):
+    return render_template('end.html', 
+                            result=result_calculate(int(size),
+                                                    int(lights), 
+                                                    int(device)
+                                                    )
+                        )
+
+@app.route('/registro', methods=['GET','POST'])
+def registro():
+    if request.method == 'GET':
+        return render_template('registro.html')
+
+app.run(debug=True)
